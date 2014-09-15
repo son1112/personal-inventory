6aa534916b955406115154c2e792432cd3af90d6
son <son@lincolnix.net>
2014-09-15 22:34:26 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalInventory::Application.config.secret_key_base = '7b9cc12bd260aa141df4877c8c7653f8443e3aaa620154dcd7bc02710ee68934ea48cf318ce976bd2bdc9636a90b85ffc5715a6198feb1f87a05a6fe30e7a93d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalInventory::Application.config.session_store :cookie_store, key: '_PersonalInventory_session'
